c0208acf783606e5ff97abba10622599931287d7
Yann Regev <yannregev@gmail.com>
2024-10-19 22:56:21 +0200

    Style change

--- include/tradfri.h
+++ include/tradfri.h
@@ -9,6 +9,6 @@ int TradfriGetAllLamps(char* response);
 int TradfriGetLamp(char* lampId, char *response);
 int TradfriTurnOnLamp(char* lampId, char* response);
 int TradfriTurnOffLamp(char* lampId, char* response);
-int TradfriDimLamp(char* lampId, int dim, char* response);
+int TradfriDimLamp(char* lampId, uint8_t dim, char* response);
 int TradfriSetLampColor(char* lampId, uint64_t color_hex, char *response);
 #endif
